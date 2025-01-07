bb97ff2c1858589357f7628b0b9a3b2180fa2d1a
Yannick Fenz'l <yannick.fenzl@icloud.com>
2025-01-07 16:50:22 +0100

    implemented choosing CPU count

--- start.sh
+++ start.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+# CPU count is first argument
+CPUS=$1
+if [ -z "$CPUS" ]; then
+  # Default to 1 CPU
+  CPUS=1
+fi
+echo "Using $CPUS CPUs"
+
 docker compose down
 docker compose up -d
 
 container_name=$(docker compose ps --format json | jq -r '.Name')
 
 docker exec $container_name make
-docker exec -it $container_name make qemu-nox
+docker exec -it $container_name make CPUS=$CPUS qemu-nox
