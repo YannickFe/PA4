7c58e60aabc7a0e31f5ac4863f99353641dd48bd
Yannick Fenz'l <yannick.fenzl@icloud.com>
2025-01-07 23:21:35 +0100

    implemented cleanup and enabled to exit qemu context

--- start.sh
+++ start.sh
@@ -8,11 +8,40 @@ if [ -z "$CPUS" ]; then
 fi
 echo "Using $CPUS CPUs"
 
-docker compose down
 docker compose up -d
 
 container_name=$(docker compose ps --format json | jq -r '.Name')
 echo "Container name: $container_name"
 
 docker exec $container_name make
-docker exec -it $container_name make CPUS=$CPUS qemu-nox
+
+# function to handle cleanup on exit
+cleanup() {
+  echo "cleanup - Stopping Docker container..."
+  docker compose down
+  exit 0
+}
+
+# trap SIGINT, SIGTERM, and EXIT signals and call cleanup
+trap cleanup SIGINT SIGTERM EXIT
+
+# create a named pipe to pass input to QEMU
+# since passing tty using docker exec -t option
+# wont allow us to catch SIGINT etc.
+PIPE=$(mktemp -u)
+mkfifo $PIPE
+
+# run QEMU in the container with input from the named pipe
+cat $PIPE | docker exec -i $container_name make CPUS=$CPUS qemu-nox &
+
+# pass input to the named pipe
+while true; do
+  read input
+  echo "$input" > $PIPE
+done
+
+# wait for QEMU process to finish
+wait $!
+
+# call cleanup on exit
+cleanup
